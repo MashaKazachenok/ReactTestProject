88c7ab10350651a7c228c594eb854d6595ffd7f2
MashaKazachenok <kazachenokm@mail.ru>
2018-01-10 17:55:09 +0300

    added rendering JSX depending on conditions.

--- js/app.js
+++ js/app.js
@@ -16,7 +16,9 @@ text: 'Absolutely free. Download...'
 var News = React.createClass({
 render: function() {
 var data = this.props.data;
-var newsTemplate = data.map(function(item, index) {
+var newsTemplate;
+if (data.length > 0) {
+newsTemplate = data.map(function(item, index) {
 return (
 <div key={index}>
 <p className="news__author">{item.author}:</p>
@@ -24,10 +26,13 @@ return (
 </div>
 )
 })
-
+} else {
+newsTemplate = <p>Unfortunately there is no news</p>
+} 
 return (
 <div className="news">
 {newsTemplate}
+<strong className={data.length > 0 ? '':'none'}>Total news: {data.length}</strong>
 </div>
 );
 }
@@ -47,7 +52,7 @@ var App = React.createClass({
 render: function() {
 return (
 <div className="app">
-Hello everyone, I'm an App component! I can display news.
+Hello everyone, I am an App component! I can display news.
 <News data={my_news} />
 <Comments />
 </div>
